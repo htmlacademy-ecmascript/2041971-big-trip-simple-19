437a1c74ea594402e6e442ae0f54e9cf5f7c2313
Лариса Зубкова <larissazubkova@gmail.com>
2023-01-08 22:35:42 +0300

    Освободит логику показа разметки от данных

--- src/view/new-point-view.js
+++ src/view/new-point-view.js
@@ -1,11 +1,12 @@
-import AbstractView from '../framework/view/abstract-view.js';
+import AbstractStatefulView from '../framework/view/abstract-stateful-view.js';
 import {BLANK_POINT, DateFormat} from '../const.js';
 import {humanizePointDate} from '../utils/point.js';
 
-const createOffers = (offers, offersModel) => offersModel.offers.map((offer) => {
-  const checked = offers.includes(offer.id) ? 'checked' : '';
+function createOffers(offers, offersModel) {
+  offersModel.offers.map((offer) => {
+    const checked = offers.includes(offer.id) ? 'checked' : '';
 
-  return `
+    return `
   <div class="event__offer-selector">
     <input class="event__offer-checkbox  visually-hidden" id="event-offer-${offer.id}" data-id="${offer.id}" type="checkbox" name="event-offer-${offer.id}" ${checked}>
     <label class="event__offer-label" for="event-offer-${offer.id}">
@@ -14,11 +15,18 @@ const createOffers = (offers, offersModel) => offersModel.offers.map((offer) => 
       <span class="event__offer-price">${offer.price}</span>
     </label>
   </div>`;
-}).join(' ');
+  }).join(' ');
+}
 
-const getPictures = (pictures) => pictures.map((picture) => `<img class="event__photo" src="${picture.src}" alt="${picture.description}"></img>`);
+function generateDestinationsList(name) {
+  //return destinations.map((destination) => `option value=${destination}></option>`).join('');
+}
 
-const createNewPointTemplate = (point, offersModel, destination) => {
+function getPictures(pictures) {
+  return pictures.map((picture) => `<img class="event__photo" src="${picture.src}" alt="${picture.description}"></img>`);
+}
+
+function createNewPointTemplate(point, offersModel, destination) {
   const {dateFrom, dateTo, offers, type, basePrice} = point;
   const {name, description, pictures} = destination;
 
@@ -34,51 +42,41 @@ const createNewPointTemplate = (point, offersModel, destination) => {
           <img class="event__type-icon" width="17" height="17" src="img/icons/${type}.png" alt="Event type icon">
         </label>
         <input class="event__type-toggle  visually-hidden" id="event-type-toggle-1" type="checkbox">
-
         <div class="event__type-list">
           <fieldset class="event__type-group">
             <legend class="visually-hidden">Event type</legend>
-
             <div class="event__type-item">
               <input id="event-type-taxi-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="taxi">
               <label class="event__type-label  event__type-label--taxi" for="event-type-taxi-1">Taxi</label>
             </div>
-
             <div class="event__type-item">
               <input id="event-type-bus-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="bus">
               <label class="event__type-label  event__type-label--bus" for="event-type-bus-1">Bus</label>
             </div>
-
             <div class="event__type-item">
               <input id="event-type-train-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="train">
               <label class="event__type-label  event__type-label--train" for="event-type-train-1">Train</label>
             </div>
-
             <div class="event__type-item">
               <input id="event-type-ship-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="ship">
               <label class="event__type-label  event__type-label--ship" for="event-type-ship-1">Ship</label>
             </div>
-
             <div class="event__type-item">
               <input id="event-type-drive-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="drive">
               <label class="event__type-label  event__type-label--drive" for="event-type-drive-1">Drive</label>
             </div>
-
             <div class="event__type-item">
               <input id="event-type-flight-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="flight" checked>
               <label class="event__type-label  event__type-label--flight" for="event-type-flight-1">Flight</label>
             </div>
-
             <div class="event__type-item">
               <input id="event-type-check-in-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="check-in">
               <label class="event__type-label  event__type-label--check-in" for="event-type-check-in-1">Check-in</label>
             </div>
-
             <div class="event__type-item">
               <input id="event-type-sightseeing-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="sightseeing">
               <label class="event__type-label  event__type-label--sightseeing" for="event-type-sightseeing-1">Sightseeing</label>
             </div>
-
             <div class="event__type-item">
               <input id="event-type-restaurant-1" class="event__type-input  visually-hidden" type="radio" name="event-type" value="restaurant">
               <label class="event__type-label  event__type-label--restaurant" for="event-type-restaurant-1">Restaurant</label>
@@ -86,19 +84,16 @@ const createNewPointTemplate = (point, offersModel, destination) => {
           </fieldset>
         </div>
       </div>
-
       <div class="event__field-group  event__field-group--destination">
-        <label class="event__label  event__type-output" for="event-destination-1">
-          Flight
-        </label>
+        <label class="event__label  event__type-output" for="event-destination-1">${type}</label>
         <input class="event__input  event__input--destination" id="event-destination-1" type="text" name="event-destination" value="${name}" list="destination-list-1">
         <datalist id="destination-list-1">
+        ${generateDestinationsList(name)}
           <option value="Amsterdam"></option>
           <option value="Geneva"></option>
           <option value="Chamonix"></option>
         </datalist>
       </div>
-
       <div class="event__field-group  event__field-group--time">
         <label class="visually-hidden" for="event-start-time-1">From</label>
         <input class="event__input  event__input--time" id="event-start-time-1" type="text" name="event-start-time" value="${startTime}">
@@ -106,7 +101,6 @@ const createNewPointTemplate = (point, offersModel, destination) => {
         <label class="visually-hidden" for="event-end-time-1">To</label>
         <input class="event__input  event__input--time" id="event-end-time-1" type="text" name="event-end-time" value="${endTime}">
       </div>
-
       <div class="event__field-group  event__field-group--price">
         <label class="event__label" for="event-price-1">
           <span class="visually-hidden">Price</span>
@@ -114,21 +108,17 @@ const createNewPointTemplate = (point, offersModel, destination) => {
         </label>
         <input class="event__input  event__input--price" id="event-price-1" type="text" name="event-price" value="${basePrice}">
       </div>
-
       <button class="event__save-btn  btn  btn--blue" type="submit">Save</button>
       <button class="event__reset-btn" type="reset">Cancel</button>
     </header>
     <section class="event__details">
       <section class="event__section  event__section--offers">
         <h3 class="event__section-title  event__section-title--offers">Offers</h3>
-
         <div class="event__available-offers">${createOffers(offers, offersModel)}</div>
       </section>
-
       <section class="event__section  event__section--destination">
         <h3 class="event__section-title  event__section-title--destination">Destination</h3>
         <p class="event__destination-description">${description}</p>
-
         <div class="event__photos-container">
           <div class="event__photos-tape">${getPictures(pictures)}</div>
         </div>
@@ -136,30 +126,69 @@ const createNewPointTemplate = (point, offersModel, destination) => {
     </section>
   </form>
 </li>`;
-};
+}
 
-export default class NewPointView extends AbstractView {
-  #point = null;
+export default class NewPointView extends AbstractStatefulView {
   #offers = null;
   #destination = null;
   #handleFormSubmit = null;
 
   constructor({point = BLANK_POINT, offers, destination, onFormSubmit}) {
     super();
-    this.#point = point;
     this.#offers = offers;
     this.#destination = destination;
+    this._setState(NewPointView.parsePointToState(point));
+
     this.#handleFormSubmit = onFormSubmit;
 
-    this.element.querySelector('form').addEventListener('submit', this.#formSubmitHandler);
+    this._restoreHandlers();
   }
 
   get template() {
-    return createNewPointTemplate(this.#point, this.#offers, this.#destination);
+    return createNewPointTemplate(this._state, this.#offers, this.#destination);
+  }
+
+  _restoreHandlers() {
+    this.element.querySelector('form').addEventListener('submit', this.#formSubmitHandler);
+    this.element.querySelector('.event__type-group').addEventListener('change', this.#typeChangeHandler);
+    this.element.querySelector('.event__input--destination').addEventListener( 'change', this.#destinationChangeHandler);
   }
 
   #formSubmitHandler = (evt) => {
     evt.preventDefault();
-    this.#handleFormSubmit(this.#point, this.#offers, this.#destination);
+    this.#handleFormSubmit(NewPointView.parseStateToPoint(this._state), this.#offers, this.#destination);
   };
+
+  #typeChangeHandler = (evt) => {
+    evt.preventDefault();
+    if (evt.target.classList.contains('event__type-input')) {
+      this.updateElement({
+        type: evt.target.value,
+        offers: [],
+      });
+    }
+  };
+
+  #destinationChangeHandler = (evt) => {
+    evt.preventDefault();
+    if (evt.target.value) {
+      const destination = this.destination.find((element) => element.name === evt.target.value) || BLANK_POINT.destination;
+      this.updateElement({
+        destination,
+      });
+      return;
+    }
+    this.updateElement(
+      BLANK_POINT.destination,
+    );
+  };
+
+  static parsePointToState(point) {
+    return {...point};
+  }
+
+  static parseStateToPoint(state) {
+    const point = {...state};
+    return point;
+  }
 }
